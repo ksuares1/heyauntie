8ecd83c34ee9fc9d8659c168ebcbfcdd886124ac
ksuares1 <kimberlyjsuares@gmail.com>
2022-08-03 17:27:33 -0400

    Added typeform

--- auntie/src/App.js
+++ auntie/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
-import LoginPage from './Pages/Login';
+
 import DiscussionBoard from './Pages/Dashboard';
 import MainProfile from './components/Profilemain';
 import Questions from './Pages/Questions';
@@ -15,7 +15,7 @@ function App() {
     <Router>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path= "/login" element={<LoginPage/>} />
+      {/* <Route path= "/login" element={<LoginPage/>} /> */}
       <Route path= "/survey" element={<Questions/>} />
       <Route path="/dashboard" element={<DiscussionBoard/>} />
       <Route path="/profilemain" element={<MainProfile/>} />
