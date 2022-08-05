ced69696bed67cabe08e52f315cb960b3fdc665c
ksuares1 <kimberlyjsuares@gmail.com>
2022-08-05 10:31:07 -0400

    added vercel

--- auntie/src/App.js
+++ auntie/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
-// import { AppRouter } from './Pages/AppRouter'
 import DiscussionBoard from './Pages/Dashboard';
 import MainProfile from './components/Profilemain';
 import Questions from './Pages/Questions';
-import Auntee from './Pages/Auntee';
+import DirectMessaging from './Pages/AunteeMessages';
 import LoginAccount from './Pages/AccountPage';
 import SignUpForm from './Pages/Createanaccount';
-import DirectMessaging from './components/DirectMessaging';
+
 
 function App() {
   return (
@@ -23,7 +22,6 @@ function App() {
           <Route path="/survey" element={<Questions />} />
           <Route path="/dashboard" element={<DiscussionBoard />} />
           <Route path="/profilemain" element={<MainProfile />} />
-          <Route path="/auntee" element={<Auntee />} />
         </Routes>
       </Router>
       </>
