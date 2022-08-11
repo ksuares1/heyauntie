f18557a9fe287cb34e481bc85a6518b873ecc8b2
ksuares1 <kimberlyjsuares@gmail.com>
2022-08-11 17:51:32 -0400

    added comet ui kit

--- auntie/src/App.js
+++ auntie/src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Pages/Home';
-import DiscussionBoard from './Pages/Dashboard';
-import Questions from './Pages/Questions';
-import LoginAccount from './Pages/AccountPage';
-import SignUpForm from './Pages/Createanaccount';
-import AunteeChatMessage from './Pages/AunteeChat';
-import CommunityLounge from './Pages/CommunityChat';
-
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import DiscussionBoard from "./Pages/Dashboard";
+import Questions from "./Pages/Questions";
+import LoginAccount from "./Pages/AccountPage";
+import SignUpForm from "./Pages/Createanaccount";
+import AunteeChatMessage from "./Pages/AunteeChat";
+import CommunityLounge from "./Pages/CommunityChat";
+import { CometChatUI } from "./cometChat/components/CometChatUI/index";
 
 function App() {
   return (
     <>
-       <Router>
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/registrationform" element={<SignUpForm/>}/> 
-          <Route path="/aunteechat" element={<AunteeChatMessage/>}/> 
-          <Route path="/communitylounge" element={<CommunityLounge/>}/>
-          <Route path="/login" element={<LoginAccount/>}/>
+          <Route path="/registrationform" element={<SignUpForm />} />
+          <Route path="/aunteechat" element={<AunteeChatMessage />} />
+          <Route path="/communitylounge" element={<CommunityLounge />} />
+          <Route path="/login" element={<LoginAccount />} />
           <Route path="/survey" element={<Questions />} />
           <Route path="/dashboard" element={<DiscussionBoard />} />
-    
+          <Route path="/latest" element={<CometChatUI />} />
         </Routes>
       </Router>
-      </>
+    </>
   );
 }
 
